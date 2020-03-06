571cb81b31958b1b7011bf3bca3140731fa3d7f3
YUKIKO IKEDA <applelele@gmail.com>
2020-03-06 22:00:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ return "HAPPY NEW YEAR!"
 end
 
 
-def countdown_with_sleep(number)
-  print "#{number} SECOND(S)!"
+def countdown_with_sleep(number2)
+  print "#{number2} SECOND(S)!"
   while number > 0
   sleep(1)
-  number -= 1
+  number2 -= 1
 end
 return "HAPPY NEW YEAR!"
 end
